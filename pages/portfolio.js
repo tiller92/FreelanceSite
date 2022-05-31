9ccad7f98858d0868c51f4ecca111d82125981c5
tiller92 <ryantiller6@gmail.com>
2022-05-31 12:40:09 -0400

    home button added

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -2,18 +2,25 @@ import Image from "next/image"
 import wealthTrac from '../public/wealthTrac.png'
 import staffingAid from '../public/staffingAid.png'
 import {AiFillGithub} from "react-icons/ai"
+import { useRouter } from "next/router"
+import {AiFillHome} from 'react-icons/ai'
 
 export default function Ppage(){
-
+  const router = useRouter()
   return (
     <>
-  <div className="bg-fuchsia-800 min-h-screen">
+  <div className="bg-indigo-400 min-h-screen">
     <div className="flex justify-center">
-  <div className="w-3/4 h-full bg-slate-100 shadow-xl shadow-slate-300 min-h-screen ">
+  <div className="w-2/4 h-full bg-slate-100 shadow-xl shadow-slate-300 min-h-screen ">
+    <button className="relatvie m-5 sm:absolute" 
+      onClick={()=>{
+        router.push('/')
+      }}
+      >  
+    
+  <AiFillHome size={30} /></button>
   <div className="flex justify-center ">
   <div className="grid grid-cols-1 w-11/12">
-    
-         
         <div className="sm:flex sm:justify-center m-5 ">
            <Image 
             className="rounded-xl"
@@ -23,17 +30,14 @@ export default function Ppage(){
              src={wealthTrac} />
         </div>
    
-
       <div className="flex justify-center h-6 m-4">
-      <button className="border border-white w-24 sm:h-8 ">full site</button>
+      <button className="border border-white w-24 sm:h-8 ">Full Site</button>
       </div>
      
-
       <div className="sm:flex sm:justify-center m-4">
       <button className=""><AiFillGithub size={40} /></button>
       </div>
      
-
       <div className="sm:flex sm:justify-evenly">
         <ul className="flex flex-col justify-evenly">
         <li> Next JS build</li>
@@ -44,7 +48,6 @@ export default function Ppage(){
         </ul>
         </div>
     
-
         <div className="sm:flex sm:justify-center">
            <Image 
             className="rounded-xl"
@@ -54,19 +57,17 @@ export default function Ppage(){
              src={staffingAid} />
         </div>
       
-
       <div className="sm:flex sm:justify-center m-4">
       <button className=""><AiFillGithub size={40} /></button>
       </div>
       
-
       <div className="sm:flex sm:justify-evenly m-5">
         <ul className="flex flex-col justify-evenly">
         <li> Python with Flask</li>
         <li> Deployed with Heroku</li>
         <li> PostgresSQL DB using SQLA ORM</li>
         <li> Styled with Boostrap</li>
-        <li> Reads Excel Files to aid with staffing</li>
+        <li> Leverages Excel Files to aid with staffing</li>
         </ul>
         </div>
   
