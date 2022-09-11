252a114e469ef8b12e4be9b26f7b914d37b192a0
tiller92 <ryantiller6@gmail.com>
2022-09-11 13:59:53 -0400

    style updated and portfolio updates

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,6 +1,8 @@
 import Image from "next/image"
 import wealthTrac from '../public/wealthTrac.png'
+import wealthTrac3 from '../public/wealthtrac3.png'
 import staffingAid from '../public/staffingAid.png'
+import congressStock from '../public/congress-stock.jpg'
 import {AiFillGithub} from "react-icons/ai"
 import { useRouter } from "next/router"
 import {AiFillHome} from 'react-icons/ai'
@@ -24,34 +26,62 @@ export default function Ppage(){
         <div className="sm:flex sm:justify-center m-5 ">
           <a target="_blank " href="https://www.wealthtrackpro.com">
            <Image 
+           alt="wealthTracker image"
             className="rounded-xl"
             height={300}
             width={300}
             objectFit='contain'
-             src={wealthTrac} />
+             src={wealthTrac3} />
              </a>
         </div>
    
       <div className="flex justify-center h-6 m-4">
-      <button target='_blank' onClick={()=>{window.open('https://www.wealthtrackpro.com')}}  className="border border-white w-24 sm:h-8 ">Full Site</button>
+      <button  target='_blank' onClick={()=>{window.open('https://www.wealthtrackpro.com')}}  className="border border-white w-24 sm:h-8 bg-slate-800 text-slate-50 ">Full Site</button>
       </div>
      
       <div className="sm:flex sm:justify-center m-4">
-      <button className=""><AiFillGithub size={40} /></button>
+      <button target='_blank' onClick={()=>{window.open('https://github.com/tiller92/WealthTracProPublic')}}   className=""><AiFillGithub size={40} /></button>
       </div>
      
       <div className="sm:flex sm:justify-evenly">
         <ul className="flex flex-col justify-evenly">
         <li> Next JS build</li>
         <li> Deployed with Vercel</li>
-        <li> PostgresSQL Database (Prisma ORM)</li>
+        <li> PostgreSQL Database (Prisma ORM)</li>
         <li> Styled with Tailwind CSS</li>
-        <li> Server Side Generation (SEO)</li>
+        <li> JSON API</li>
+        <li> Personal Finance </li>
         </ul>
         </div>
     
         <div className="sm:flex sm:justify-center">
            <Image 
+           alt="congress stock image"
+            className="rounded-xl"
+            height={300}
+            width={300}
+            objectFit='contain'
+             src={congressStock} />
+        </div>
+        <div className="flex justify-center h-6 m-4">
+      <button target='_blank' onClick={()=>{window.open('https://congress-stock-tracker.herokuapp.com/')}}  className="border border-white w-24 sm:h-8 bg-slate-800 text-slate-50 ">Full Site</button>
+      </div>
+      <div className="sm:flex sm:justify-center m-4">
+      <button onClick={()=>{window.open('https://github.com/tiller92/capstone-ryan')}} className=""><AiFillGithub size={40} /></button>
+      </div>
+      
+      <div className="sm:flex sm:justify-evenly m-5">
+        <ul className="flex flex-col justify-evenly">
+        <li> Python Flask</li>
+        <li> Deployed with Heroku</li>
+        <li> PostgresSQL DB using SQLA ORM</li>
+        <li> Styled with Boostrap</li>
+        <li> House Stock Wathcer JSON API</li>
+        </ul>
+        </div>
+        <div className="sm:flex sm:justify-center">
+           <Image 
+           alt="rt-assigment tracler image"
             className="rounded-xl"
             height={300}
             width={300}
@@ -60,12 +90,12 @@ export default function Ppage(){
         </div>
       
       <div className="sm:flex sm:justify-center m-4">
-      <button className=""><AiFillGithub size={40} /></button>
+      <button target='_blank' onClick={()=>{window.open('https://github.com/tiller92/rt-assignment-tracker')}} className=""><AiFillGithub size={40} /></button>
       </div>
       
       <div className="sm:flex sm:justify-evenly m-5">
         <ul className="flex flex-col justify-evenly">
-        <li> Python with Flask</li>
+        <li> Python Flask</li>
         <li> Deployed with Heroku</li>
         <li> PostgresSQL DB using SQLA ORM</li>
         <li> Styled with Boostrap</li>
