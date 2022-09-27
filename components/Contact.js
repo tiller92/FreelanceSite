941213fb649e6fcaa571ad82836e8c5273f5b28c
tiller92 <ryantiller6@gmail.com>
2022-09-26 20:23:26 -0400

    contact button fixed

--- components/Contact.js
+++ components/Contact.js
@@ -7,8 +7,7 @@ export default function Contact({open}){
     <>
     <div className="bg-white w-fit rounded-xl">
       <p className=" p-2 font-sans-apple-system text-purple-500">
-        Currently taking projects!
-        Email: ryantiller6@gmail.com
+       ryantiller6@gmail.com
       </p>
     </div>
     </>
